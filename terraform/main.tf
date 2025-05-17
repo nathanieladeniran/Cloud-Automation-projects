ac63629d08897b0a538430af4e048335b0b5cd3b
nathanieladeniran <nathanieladeniran@gmail.com>
2025-05-17 16:17:59 +0100

    Added more resources to be created to terraform code,added var.tf, version.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,11 +43,14 @@ data "aws_ami" "ubuntu" {
 # VPC creation
 # --------------------------------------------------------------
 
-resource "aws_vpc" "Nath-VPC" {
-  cidr_block           = "10.0.0.0/16"
+resource "aws_vpc" "Quiva-VPC" {
+  cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true #DNS name won’t show unless DNS hostnames are enabled on your VPC with this line
   tags = {
-    Name = "Nath_vpc"
+    Name = "Quiva_vpc"
+    Purpose = element(var.Purpose, 1)
+    Environment = element(var.Environment, 1)
+    Deployed-By = element(var.Deployed-by, 1)
   }
 }
 
@@ -55,13 +58,17 @@ resource "aws_vpc" "Nath-VPC" {
 # Public Subnet for Bastion Server / Jump Host creation
 # --------------------------------------------------------------
 
-resource "aws_subnet" "Nath_public_subnet" {
-  vpc_id                  = aws_vpc.Nath-VPC.id
-  cidr_block              = "10.0.0.0/24"
-  availability_zone       = "us-east-1a"
+resource "aws_subnet" "Quiva_public_subnet" {
+  vpc_id                  = aws_vpc.Quiva-VPC.id
+  cidr_block              = cidrsubnet(aws_vpc.Quiva-VPC.cidr_block, 8, count.index)
+  count                   = length(var.availability_zone)
+  availability_zone       = element(var.availability_zone, count.index)
   map_public_ip_on_launch = true #Only used when the subnet is for the public
   tags = {
-    Name = "Nath_public_subnet_1a"
+    Name = "Quiva_public_subnet_${substr(element(var.availability_zone, count.index), -2, 2)}"
+    Purpose = element(var.Purpose, 1)
+    Environment = element(var.Environment, 1)
+    Deployed-By = element(var.Deployed-by, 1)
   }
 }
 
@@ -69,10 +76,10 @@ resource "aws_subnet" "Nath_public_subnet" {
 # Internet Gateway creation
 # --------------------------------------------------------------
 
-resource "aws_internet_gateway" "Nath_igw" {
-  vpc_id = aws_vpc.Nath-VPC.id
+resource "aws_internet_gateway" "Quiva_igw" {
+  vpc_id = aws_vpc.Quiva-VPC.id
   tags = {
-    Name = "Nath_igw"
+    Name = "Quiva_igw"
   }
 }
 
@@ -80,11 +87,11 @@ resource "aws_internet_gateway" "Nath_igw" {
 # Route Table creation
 # --------------------------------------------------------------
 
-resource "aws_route_table" "Nath_route_table" {
-  vpc_id = aws_vpc.Nath-VPC.id
+resource "aws_route_table" "Quiva_route_table" {
+  vpc_id = aws_vpc.Quiva-VPC.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.Nath_igw.id
+    gateway_id = aws_internet_gateway.Quiva_igw.id
   }
 }
 
@@ -92,36 +99,48 @@ resource "aws_route_table" "Nath_route_table" {
 # Route Table Association
 # --------------------------------------------------------------
 
-resource "aws_route_table_association" "Nath_table_assoc" {
-  route_table_id = aws_route_table.Nath_route_table.id
-  subnet_id      = aws_subnet.Nath_public_subnet.id
+resource "aws_route_table_association" "public_internet_server" {
+  count = length(aws_subnet.Quiva_public_subnet)
+  route_table_id = aws_route_table.Quiva_route_table.id
+  subnet_id      = element(aws_subnet.Quiva_public_subnet.*.id, count.index)
 }
 
 # --------------------------------------------------------------
-# Key pair creation
+# Key pair creation and storing
 # --------------------------------------------------------------
 
-# SSH Key
+# creating the key pair
 resource "tls_private_key" "ssh_key" {
   algorithm = "RSA"
   rsa_bits  = 4096
 }
-resource "aws_key_pair" "Nath_key" {
-  key_name   = "Nath-Key"
-  public_key = file("${path.module}/Nath-Key.pub") #file("~/.ssh/Nath-Key.pub")  # first generate the public key (ssh-keygen -t rsa -b 4096 -f ~/.ssh/Nath-Key) then access it with this line
+
+# This creates a private key file
+resource "local_file" "Quiva_private_key" {
+  content         = tls_private_key.ssh_key.private_key_pem
+  filename        = "${path.module}/Quiva-Key-Pair.pem"
+  file_permission = "0400"
+}
+resource "aws_key_pair" "Quiva_bastion_key" {
+  key_name   = "Quiva-Key-Pair"
+  public_key = tls_private_key.ssh_key.public_key_openssh
+  # public_key = file("${path.module}/Quiva-Key.pub") #file("~/.ssh/Quiva-Key.pub")  # first generate the public key (ssh-keygen -t rsa -b 4096 -f ~/.ssh/Quiva-Key) then access it with this line
 }
 
 # --------------------------------------------------------------
 # Security Group creation
 # --------------------------------------------------------------
 
-resource "aws_security_group" "Nath_Bastion_sg" {
-  vpc_id      = aws_vpc.Nath-VPC.id
-  name        = "Allow SSH"
+resource "aws_security_group" "Quiva_Bastion_sg" {
+  vpc_id      = aws_vpc.Quiva-VPC.id
+  name        = "Quiva Public SG"
   description = "Allow traffic from ssg"
 
   tags = {
-    Name = "Nath_sg"
+    Name = "Quiva_public_sg"
+    Purpose = element(var.Purpose, 1)
+    Environment = element(var.Environment, 1)
+    Deployed-By = element(var.Deployed-by, 1)
   }
 
   ingress {
@@ -159,16 +178,35 @@ resource "aws_security_group" "Nath_Bastion_sg" {
 # Public EC2 instance creation
 # --------------------------------------------------------------
 
-resource "aws_instance" "Nath_Bastion_server" {
+resource "aws_instance" "Quiva_Bastion_server" {
   ami                         = data.aws_ami.amazon_linux.id
-  vpc_security_group_ids      = [aws_security_group.Nath_Bastion_sg.id]
-  subnet_id                   = aws_subnet.Nath_public_subnet.id
+  vpc_security_group_ids      = [aws_security_group.Quiva_Bastion_sg.id]
+  subnet_id                   = aws_subnet.Quiva_public_subnet[0].id
   instance_type               = "t2.micro"
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.Nath_key.key_name
+  key_name                    = aws_key_pair.Quiva_bastion_key.key_name
   # key_name = "S-key"  #use this if the key has been created before and its available on the aws console 
+
+  user_data   = <<-EOF
+
+  #!/bin/bash
+
+  # install httpd (Linux 2 version)
+  yum update -y
+  yum install -y httpd
+  systemctl start httpd
+  systemctl enable httpd
+  sudo usermod -a -G apache ec2-user 
+  sudo chown -R ec2-user:apache /var/www
+  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+
+  EOF
+
   tags = {
-    Name = "Nath_server"
+    Name = "Quiva_public_server"
+    Purpose = element(var.Purpose, 1)
+    Environment = element(var.Environment, 1)
+    Deployed-By = element(var.Deployed-by, 1)
   }
 }
 
@@ -177,38 +215,46 @@ resource "aws_instance" "Nath_Bastion_server" {
 # --------------------------------------------------------------
 # Private Subnet creation
 # --------------------------------------------------------------
-resource "aws_subnet" "Nath_private_subnet" {
-  vpc_id            = aws_vpc.Nath-VPC.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+resource "aws_subnet" "Quiva_private_subnet" {
+  vpc_id            = aws_vpc.Quiva-VPC.id
+  count = length(var.availability_zone)
+  cidr_block        = cidrsubnet(aws_vpc.Quiva-VPC.cidr_block, 8, count.index + 10)
+  availability_zone = element(var.availability_zone, count.index)
+  map_public_ip_on_launch = false
   tags = {
-    Name = "Nath_private_subnet_1a"
+    Name = "Quiva_private_subnet_${substr(element(var.availability_zone, count.index), -2, 2)}"
+    Purpose = element(var.Purpose, 0)
+    Environment = element(var.Environment, 1)
+    Deployed-By = element(var.Deployed-by, 1)
   }
 }
 
 # --------------------------------------------------------------
 # NAT Gateway creation
 # --------------------------------------------------------------
-resource "aws_eip" "Nath_nat_eip" {
+resource "aws_eip" "Quiva_nat_eip" {
   tags = {
-    Name = "Nath_Nat_EIP"
+    Name = "Quiva_Nat_EIP"
+    Purpose = element(var.Purpose, 0)
+    Environment = element(var.Environment, 1)
+    Deployed-By = element(var.Deployed-by, 1)
   }
 }
 
-resource "aws_nat_gateway" "Nath_nat_gateway" {
-  allocation_id = aws_eip.Nath_nat_eip.id
-  subnet_id     = aws_subnet.Nath_private_subnet.id
+resource "aws_nat_gateway" "Quiva_nat_gateway" {
+  allocation_id = aws_eip.Quiva_nat_eip.id
+  subnet_id     = aws_subnet.Quiva_public_subnet[0].id
 }
 
 # --------------------------------------------------------------
 # Private Route Table
 # --------------------------------------------------------------
 
-resource "aws_route_table" "Nath_private_route_table" {
-  vpc_id = aws_vpc.Nath-VPC.id
+resource "aws_route_table" "Quiva_private_route_table" {
+  vpc_id = aws_vpc.Quiva-VPC.id
   route {
     cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.Nath_nat_gateway.id
+    nat_gateway_id = aws_nat_gateway.Quiva_nat_gateway.id
   }
 
 }
@@ -216,19 +262,22 @@ resource "aws_route_table" "Nath_private_route_table" {
 # --------------------------------------------------------------
 # Private Security Group
 # --------------------------------------------------------------
-resource "aws_security_group" "Nath_private_sg" {
-  vpc_id      = aws_vpc.Nath-VPC.id
+resource "aws_security_group" "Quiva_private_sg" {
+  vpc_id      = aws_vpc.Quiva-VPC.id
   description = "Allow SSH from Bastion host alone"
-  name        = "Nath Private SG"
+  name        = "Quiva Private SG"
   tags = {
-    Name = "Nath_private_sg"
+    Name = "Quiva_private_sg"
+    Purpose = element(var.Purpose, 0)
+    Environment = element(var.Environment, 1)
+    Deployed-By = element(var.Deployed-by, 1)
   }
 
   ingress {
     from_port       = 22
     to_port         = 22
     protocol        = "tcp"
-    security_groups = [aws_security_group.Nath_Bastion_sg.id]
+    security_groups = [aws_security_group.Quiva_Bastion_sg.id]
   }
 
   egress {
@@ -244,16 +293,19 @@ resource "aws_security_group" "Nath_private_sg" {
 # Private EC2 instance/server
 # --------------------------------------------------------------
 
-resource "aws_instance" "Nath_private_server" {
+resource "aws_instance" "Quiva_private_server" {
   ami                         = data.aws_ami.amazon_linux.id
-  vpc_security_group_ids      = [aws_security_group.Nath_private_sg.id]
+  vpc_security_group_ids      = [aws_security_group.Quiva_private_sg.id]
   instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.Nath_private_subnet.id
+  subnet_id                   = aws_subnet.Quiva_private_subnet[0].id
   associate_public_ip_address = false
-  key_name                    = aws_key_pair.Nath_key.key_name
+  key_name                    = aws_key_pair.Quiva_bastion_key.key_name
 
   tags = {
-    Name = "Nath_private_server"
+    Name = "Quiva_private_server"
+    Purpose = element(var.Purpose, 0)
+    Environment = element(var.Environment, 1)
+    Deployed-By = element(var.Deployed-by, 1)
   }
 
 }
---
+++ terraform/var.tf
@@ -0,0 +1,28 @@
+
+variable "AWS_REGION" {
+  default = "us-east-1"
+}
+
+variable "availability_zone" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "vpc_cidr_block" {
+  default = "10.0.0.0/16"
+}
+
+variable "Environment" {
+  type = list(string)
+  default = [ "test", "staging", "production" ]
+}
+
+variable "Purpose" {
+  type = list(string)
+  default = [ "private", "public" ]
+}
+
+variable "Deployed-by" {
+    type = list(string)
+    default = [ "Enigneer", "Senior Engineer" ]
+}
